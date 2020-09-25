9537340434193160963807854dc87cda7903d34d
ksyeon <cathy@dvarena.co.kr>
2020-09-25 18:52:18 +0900

    p.74 스레드 생성

--- app/src/main/java/kr/co/example/rssreader/MainActivity.kt
+++ app/src/main/java/kr/co/example/rssreader/MainActivity.kt
@@ -2,10 +2,22 @@ package kr.co.example.rssreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+
+    // 클래스 레벨에서 디스패처 생성
+    private val netDispatcher = newSingleThreadContext(name = "ServiceCall")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 특정 디스패처로 코루틴 실행
+        GlobalScope.launch(netDispatcher) {
+            // TODO Call coroutine here
+        }
     }
 }
